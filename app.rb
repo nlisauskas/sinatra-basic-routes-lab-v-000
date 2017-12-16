9782df3842a25570e71e2955ed1fa80eb761759c
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-12-16 11:06:43 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Nick"
+  end
+
+  get '/hometown' do
+    "My hometown is Aurora, Illinois."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Midnight Rambler by the Rolling Stones."
+  end
 end
